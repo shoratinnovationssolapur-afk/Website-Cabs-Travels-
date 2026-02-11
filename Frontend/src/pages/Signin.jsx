894e3b25975469696f2563024681ab88cd17b400
PrachiNadgir <nadgirprachi@gmail.com>
2026-02-11 14:48:14 +0530

    made changes in Signin Page

--- Frontend/src/pages/Signin.jsx
+++ Frontend/src/pages/Signin.jsx
@@ -44,33 +44,33 @@ const Signin = () => {
       <div className="absolute bottom-12 w-full border-t-4 border-dashed border-yellow-400"></div>
 
      
-      <form className="relative z-10 w-[90%] h-[300px] max-w-[500px] bg-white/20 backdrop-blur-md shadow-xl shadow-blue-900/30 flex flex-col justify-center items-center gap-[20px] px-[20px] rounded-2xl">
-        <h1 className="text-white text-[30px] font-semibold font-serif mt-[50px] text-center">
+      <form className="relative z-10 w-[90%] h-[500px] max-w-[500px] bg-white/20  shadow-xl shadow-blue-900/30 flex flex-col justify-center items-center gap-[20px] px-[20px] rounded-2xl">
+        <h1 className={`${isNight?"text-white":"text-black"} text-[30px] font-semibold font-serif  text-center`}>
           Sign In To{" "}
-          <span className="text-yellow-400">Rathod Cabs And Travels</span>
+          <span className={`${isNight ? "text-yellow-300" : "text-blue-600"}`}>Rathod Cabs And Travels</span>
         </h1>
 
         <input
           type="email"
           placeholder="Email"
-          className="w-full h-[60px] outline-0 border-2 border-white bg-transparent text-black placeholder-black px-[20px] py-[10px] rounded-full text-[15px]"
+          className={`w-full h-[60px] outline-0 border-2 border-white bg-transparent text-black ${isNight?"placeholder-white":"placeholder-black"} px-[20px] py-[10px] rounded-full text-[15px]`}
         />
 
         <div className="w-full h-[60px] relative">
           <input
             type={showpassword ? "text" : "password"}
             placeholder="Password"
-            className="w-full h-full outline-0 border-2 border-white bg-transparent text-black placeholder-black px-[20px] py-[10px] rounded-full text-[15px]"
+            className={`w-full h-full outline-0 border-2 border-white bg-transparent text-black ${isNight?"placeholder-white":"placeholder-black"} px-[20px] py-[10px] rounded-full text-[15px]`}
           />
 
           {showpassword ? (
             <IoEyeOff
-              className="absolute bottom-[9px] right-[40px] text-white w-[25px] h-[25px] cursor-pointer"
+              className="absolute bottom-[16px] right-[40px] text-white w-[25px] h-[25px] cursor-pointer"
               onClick={() => setshowpassword(false)}
             />
           ) : (
             <IoEye
-              className="absolute bottom-[9px] right-[40px] text-white w-[25px] h-[25px] cursor-pointer"
+              className="absolute bottom-[16px] right-[40px] text-white w-[25px] h-[25px] cursor-pointer"
               onClick={() => setshowpassword(true)}
             />
           )}
@@ -78,17 +78,17 @@ const Signin = () => {
 
         <button
           type="button"
-          className="min-w-[130px] h-[50px] bg-white rounded-full mt-[20px] text-[19px] font-bold "
+          className=" absolute bottom-[10px] min-w-[130px] h-[50px] bg-white rounded-full mt-[20px] text-[19px] font-bold "
         >
           Sign In
         </button>
 
         <p
-          className="text-white cursor-pointer"
+          className={`${isNight?"text-white":"text-black"} cursor-pointer`}
           onClick={() => navigate("/signup")}
         >
           Want to create a new account?{" "}
-          <span className="text-blue-800 font-semibold">Sign Up</span>
+          <span className={`${isNight?"text-yellow-300":"text-blue-600"} font-semibold`}>Sign Up</span>
         </p>
       </form>
     </div>
