6a1b6d8bf9e5e28356782b74020027b9faa43e9d
PrachiNadgir <nadgirprachi@gmail.com>
2026-02-18 16:20:11 +0530

    changes in homepage,bookingdetails,app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -38,18 +38,13 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/rent-your-car", element: <RentCarPage /> },
+
+      // ⭐ ADD BOOKING PAGE HERE
+      { path: "/booking", element: <BookingDetails /> },
     ],
   },
-  {
-      
-        path : "/booking-details",
-        element : <BookingDetails />
-      
-  }
-
- ,
- 
 ]);
+
 
 
 function App() {
